05b0a18bb84c21905849b9e943e0c7e06cc897f9
Emandleni <emandlenimoyo@gmail.com>
2024-04-21 18:25:51 +0200

    Implemented FindNearestElevator method for more efficient elevator selection

--- dvt_elevator_challenge_solution/dvt_elevator_challenge_solution/Building.cs
+++ dvt_elevator_challenge_solution/dvt_elevator_challenge_solution/Building.cs
@@ -46,6 +46,41 @@ namespace dvt_elevator_challenge_solution
             }
         }
 
+        private Elevator FindNearestElevator(List<Elevator> elevators, int requestedFloor, Type elevatorType, double goodsWeight, int passengerCount, Elevator nearestElevator, int shortestDistance)
+        {
+            // Base case: If there are no more elevators to consider, return the nearest elevator found so far
+            if (elevators.Count == 0)
+            {
+                return nearestElevator;
+            }
+
+            var elevator = elevators[0];
+
+            int distance = Math.Abs(elevator.CurrentFloor - requestedFloor);
+
+            if (!elevator.IsMoving || elevator.Direction == ElevatorDirection.Stationary)
+            {
+                // Check if the elevator can accommodate passengers or goods based on the type
+                bool canAccommodate = (elevator is PassengerElevator && ((PassengerElevator)elevator).PassengerCount + passengerCount <= ((PassengerElevator)elevator).MaxPassengerLimit) ||
+                                      (elevator is GoodsElevator && ((GoodsElevator)elevator).WeightCount + goodsWeight <= ((GoodsElevator)elevator).maxWeightLimitInKgs);
+
+                if (canAccommodate)
+                {
+                    // Update nearest elevator if it's closer to the requested floor
+                    if (distance < shortestDistance)
+                    {
+                        nearestElevator = elevator;
+                        shortestDistance = distance;
+                    }
+                }
+            }
+
+            // Move to the next elevator in the list and recursively call FindNearestElevator
+            return FindNearestElevator(elevators.GetRange(1, elevators.Count - 1), requestedFloor, elevatorType, goodsWeight, passengerCount, nearestElevator, shortestDistance);
+        }
+
+
+
     }
 }
 
