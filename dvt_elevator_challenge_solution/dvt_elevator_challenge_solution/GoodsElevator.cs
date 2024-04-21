f181ab0ad94bba1817ee405ac8337d181e09a9a5
Emandleni <emandlenimoyo@gmail.com>
2024-04-21 18:10:21 +0200

    Implemented PassengerElevator and GoodsElevator classes

---
+++ dvt_elevator_challenge_solution/dvt_elevator_challenge_solution/GoodsElevator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace dvt_elevator_challenge_solution
+{
+    public class GoodsElevator : Elevator
+    {
+        public double WeightCount { get; protected set; }
+        public double maxWeightLimitInKgs;
+
+        public GoodsElevator(double maxWeightLimitInKgs, int elevatorID) : base(elevatorID)
+        {
+            this.maxWeightLimitInKgs = maxWeightLimitInKgs;
+            this.WeightCount = 0;
+            speed = 50;
+
+        }
+
+        public override void Load(double weightInKgs)
+        {
+            // Implementation specific to goods elevators
+            // Only load goods onto the elevator if the weight does not exceed the maximum weight limit
+            if (weightInKgs <= maxWeightLimitInKgs)
+            {
+                Console.WriteLine($"Loading {weightInKgs} kilogram units of goods into goods elevator {ElevatorID}");
+                WeightCount += weightInKgs;
+            }
+            else
+            {
+                Console.WriteLine("Cannot load goods. Maximum weight limit exceeded.");
+            }
+        }
+
+        public override void Unload()
+        {
+            // Implementation specific to goods elevators
+            Console.WriteLine($"Unloading {WeightCount} kilogram units of goods from the goods elevator {ElevatorID}");
+            WeightCount = 0;
+            Console.WriteLine("Unloaded");
+        }
+
+        public override void DisplayStatus()
+        {
+            Console.WriteLine($"Elevator ID: {ElevatorID}, Current Floor: {CurrentFloor}, " +
+                              $"Direction: {Direction}, Current Weight (kg): {WeightCount}, Max Weight Limit (kg): {maxWeightLimitInKgs}");
+        }
+    }
+}
+
---
+++ dvt_elevator_challenge_solution/dvt_elevator_challenge_solution/PassengerElevator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace dvt_elevator_challenge_solution
+{
+    public class PassengerElevator : Elevator
+    {
+        public int PassengerCount { get; protected set; }
+        public int MaxPassengerLimit { get; }
+
+        public PassengerElevator(int maxPassengerLimit, int elevatorID) : base(elevatorID)
+        {
+            MaxPassengerLimit = maxPassengerLimit;
+            PassengerCount = 0;
+            speed = 100;
+        }
+
+        public override void Load(int passengerCount)
+        {
+            if (PassengerCount + passengerCount <= MaxPassengerLimit)
+            {
+                Console.WriteLine($"Loading {passengerCount} passengers into passenger elevator {ElevatorID}");
+                PassengerCount += passengerCount;
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot load passengers. Elevator is at full capacity.");
+            }
+        }
+
+        public override void Unload()
+        {
+            // Implementation specific to passenger elevators
+            Console.WriteLine($"Unloading {PassengerCount} passengers from passenger elevator {ElevatorID}");
+            PassengerCount = 0;
+            Console.WriteLine("Unloaded");
+        }
+
+        public override void DisplayStatus()
+        {
+            Console.WriteLine($"Elevator ID: {ElevatorID}, Current Floor: {CurrentFloor}, " +
+                              $"Direction: {Direction}, Passenger Count: {PassengerCount}, Max Passenger Limit: {MaxPassengerLimit}");
+        }
+    }
+}
+
