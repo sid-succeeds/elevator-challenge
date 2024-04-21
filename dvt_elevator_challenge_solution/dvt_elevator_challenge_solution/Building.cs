b47f61325b2919da2539b6c6aec69439417063a5
Emandleni <emandlenimoyo@gmail.com>
2024-04-21 18:18:05 +0200

    Defined basic Building class members

---
+++ dvt_elevator_challenge_solution/dvt_elevator_challenge_solution/Building.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace dvt_elevator_challenge_solution
+{
+    public class Building
+    {
+        private List<Elevator> elevators;
+        private int totalFloors;
+
+        public Building(int numberOfFloors)
+        {
+            totalFloors = numberOfFloors;
+            elevators = new List<Elevator>();
+        }
+
+        public void AddElevator(Elevator elevator)
+        {
+            elevators.Add(elevator);
+        }
+
+    }
+}
+
