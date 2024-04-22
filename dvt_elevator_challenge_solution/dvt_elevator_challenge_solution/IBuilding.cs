2e0981c628c13dcd56ca9ca2bb37cb237aa57a90
Emandleni <emandlenimoyo@gmail.com>
2024-04-22 13:37:30 +0200

    Introduced IBuilding interface

---
+++ dvt_elevator_challenge_solution/dvt_elevator_challenge_solution/IBuilding.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace dvt_elevator_challenge_solution
+{
+	public interface IBuilding
+	{
+        void AddElevator(IElevator elevator);
+        void CallElevator(int requestedFloor, Type elevatorType);
+        void DisplayAllElevatorStatus(Type elevatorType = null);
+    }
+}
+
